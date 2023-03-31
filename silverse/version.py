db4562d2e137c76de376839093272ca5d5647d02
ermiry <erick.salas@ermiry.com>
2023-03-30 22:48:45 -0600

    Added methods to keep track of version

---
+++ silverse/version.py
@@ -0,0 +1,43 @@
+from cerver.utils.log import LOG_TYPE_NONE, cerver_log_both
+
+from .lib import lib
+
+SILVERSE_VERSION = "0.1"
+SILVERSE_VERSION_NAME = "Version 0.1"
+SILVERSE_VERSION_DATE = "30/03/2023"
+SILVERSE_VERSION_TIME = "22:46 CST"
+SILVERSE_VERSION_AUTHOR = "Erick Salas"
+
+version = {
+	"id": SILVERSE_VERSION,
+	"name": SILVERSE_VERSION_NAME,
+	"date": SILVERSE_VERSION_DATE,
+	"time": SILVERSE_VERSION_TIME,
+	"author": SILVERSE_VERSION_AUTHOR
+}
+
+silverse_libauth_version_print_full = lib.silverse_libauth_version_print_full
+silverse_libauth_version_print_version_id = lib.silverse_libauth_version_print_version_id
+silverse_libauth_version_print_version_name = lib.silverse_libauth_version_print_version_name
+
+def pysilverse_version_print_full ():
+	output = "\nPySilverse Version: {name}\n" \
+		"Release Date: {date} - {time}\n" \
+		"Author: {author}\n".format (**version)
+
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		output.encode ("utf-8")
+	)
+
+def pysilverse_version_print_version_id ():
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		f"\nPySilverse Version ID: {version.id}\n".encode ("utf-8")
+	)
+
+def pysilverse_version_print_version_name ():
+	cerver_log_both (
+		LOG_TYPE_NONE, LOG_TYPE_NONE,
+		f"\nPySilverse Version: {version.name}\n".encode ("utf-8")
+	)
